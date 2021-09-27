1348c014fe63959184d9fcd380d389cf8e6bfcc8
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-09-27 19:39:09 +0200

    feat: add API query class

--- src/js/ExcursionsAPI.js
+++ src/js/ExcursionsAPI.js
@@ -1,5 +1,47 @@
 class ExcursionsAPI {
+    constructor() {
+        this.url = 'http://locahost:3000/';
+    }
 
+    loadData() {
+        return this._fetch('excursions');
+    }
+
+    addData(path, data) {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        return this._fetch(path, options);
+    }
+
+    removeData(id) {
+        const options = { method: 'DELETE' };
+        return this._fetch(`excursions/${id}`, options);
+    }
+
+    updateData(id, data) {
+        const options = {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        return this._fetch(`excursions/${id}`, options);
+    }
+
+    _fetch(additionalPath, options) {
+        const path = this.url + additionalPath;
+        return fetch(path, options)
+            .then(resp => {
+                if (resp.ok) { return resp.json() }
+                return Promise.reject(resp);
+            });
+    }
 }
 
 export default ExcursionsAPI;
